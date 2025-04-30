092e1f5a138a2f52cd5d0f009ff1c5ebea1cdc21
Rodrigo Quelhas <22591718+RomarQ@users.noreply.github.com>
2025-04-30 08:30:59 +0100

    Expose origin and the addresses of contracts being constructed in precompile handle

--- src/executor/stack/executor.rs
+++ src/executor/stack/executor.rs
@@ -1595,6 +1595,11 @@ impl<'inner, 'config, 'precompiles, S: StackState<'config>, P: PrecompileSet> Pr
 		self.context
 	}
 
+	/// Retreive the address of the EOA that originated the transaction.
+	fn origin(&self) -> H160 {
+		self.executor.state.origin()
+	}
+
 	/// Is the precompile call is done statically.
 	fn is_static(&self) -> bool {
 		self.is_static
@@ -1603,5 +1608,10 @@ impl<'inner, 'config, 'precompiles, S: StackState<'config>, P: PrecompileSet> Pr
 	/// Retreive the gas limit of this call.
 	fn gas_limit(&self) -> Option<u64> {
 		self.gas_limit
+	}
+
+	/// Check if a given address is a contract being constructed
+	fn is_contract_being_constructed(&self, address: H160) -> bool {
+		self.executor.state.created(address)
 	}
 }
