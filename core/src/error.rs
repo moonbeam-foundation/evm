6a30c9941ce872e9e2bdd34fa38154cc793b9958
Manuel Mauro <manuel.mauro@protonmail.com>
2025-06-17 13:47:47 +0300

    feat: :sparkles: implemnt DecodeWithMemTracking on ExitReason

--- core/src/error.rs
+++ core/src/error.rs
@@ -18,7 +18,12 @@ pub enum Capture<E, T> {
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[cfg_attr(
 	feature = "with-codec",
-	derive(scale_codec::Encode, scale_codec::Decode, scale_info::TypeInfo)
+	derive(
+		scale_codec::Encode,
+		scale_codec::Decode,
+		scale_codec::DecodeWithMemTracking,
+		scale_info::TypeInfo
+	)
 )]
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum ExitReason {
